3811caff67a7e71cc3a8036e40e38511f41ea09d
Ethan <ethanhook2@outlook.com>
2023-06-26 19:00:30 +0100

    modified:   app/globals.css 	modified:   app/layout.tsx 	modified:   app/page.tsx 	new file:   components/CustomButton.tsx 	new file:   components/CustomFilter.tsx 	new file:   components/Footer.tsx 	new file:   components/Hero.tsx 	new file:   components/Navbar.tsx 	new file:   components/SearchBar.tsx 	new file:   components/SearchManufacturer.tsx 	new file:   constants/index.ts 	modified:   package-lock.json     modified:   package.json 	new file:   public/arrow-down.svg 	new file:   public/car-logo.svg 	new file:   public/chevron-up-down.svg 	new file:   public/close.svg 	new file:   public/discord.svg 	new file:   public/facebook.svg 	new file:   public/gas.svg 	new file:   public/github.svg 	new file:   public/heart-filled.svg 	new file:   public/heart-outline.svg 	new file:   public/hero-bg.png 	new file:   public/hero.png 	new file:   public/linkedin.svg 	new file:   public/logo.svg 	new file:   public/magnifying-glass.svg 	new file:   public/model-icon.png 	new file:   public/pattern.png 	new file:   public/right-arrow.svg 	new file:   public/steering-wheel.svg 	new file:   public/tire.svg 	new file:   public/twitter.svg 	modified:   tailwind.config.js 	new file:   types/index.ts

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+type Props = {}
+
+function CustomFilter({}: Props) {
+  return (
+    <div>CustomFilter</div>
+  )
+}
+
+export default CustomFilter
---
+++ components/SearchBar.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import SearchManufacturer from "./SearchManufacturer";
+
+
+type Props = {};
+
+function SearchBar({}: Props) {
+  const [manufacturer, setManuFacturer] = useState("");
+  const handleSearch = () => {};
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        {/* SearchManufacturer */}
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManuFacturer={setManuFacturer}
+        />
+      </div>
+    </form>
+  );
+}
+
+export default SearchBar;
