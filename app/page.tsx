cde6bf164b49fa0b91d8cee5f5c8189fc300dff5
Ethan <ethanhook2@outlook.com>
2023-06-27 22:17:18 +0100

    modified:   app/favicon.ico 	modified:   app/page.tsx 	modified:   components/CarCard.tsx 	new file:   components/CarDetails.tsx 	modified:   components/CustomButton.tsx 	modified:   components/CustomFilter.tsx 	modified:   components/SearchBar.tsx 	new file:   components/ShowMore.tsx 	modified:   next.config.js 	modified:   package-lock.json 	modified:   package.json 	modified:   tailwind.config.js 	modified:   utils/index.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
+// Date: 27/06/23
 import CarCard from "@/components/CarCard";
 import CustomFilter from "@/components/CustomFilter";
 import Hero from "@/components/Hero";
 import SearchBar from "@/components/SearchBar";
+import ShowMore from "@/components/ShowMore";
+import { fuels, yearsOfProduction } from "@/constants";
+import { HomeProps } from "@/types";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
@@ -27,8 +37,8 @@ export default async function Home() {
 
           <div className="home__filter-container">
             {/* CustomFilter */}
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
@@ -38,9 +48,14 @@ export default async function Home() {
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car) => (
-                <CarCard />
+                <CarCard car={car} />
               ))}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,18 +4,28 @@ import Image from "next/image";
 import { CustomButtonProps } from "@/types";
 
 function CustomButton({
-    title,
-    containerStyles,
-    handleClick
+  title,
+  containerStyles,
+  handleClick,
+  textStyles,
+  rightIcon,
+  btnType
 }: CustomButtonProps) {
   return (
     <button
       disabled={false}
-      type={"button"}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles} `}>
+        {title}
+      </span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image src={rightIcon} alt="right-icon" fill className="object-contain" />
+        </div>
+      )}
     </button>
   );
 }
