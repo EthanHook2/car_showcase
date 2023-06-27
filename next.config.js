6ae7ba20795517c83bf0a7f2c889d25bdeb615eb
Ethan <ethanhook2@outlook.com>
2023-06-27 22:40:01 +0100

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['cdn.imagin.studio'],
-    }
-}
+  images: {
+    domains: ["cdn.imagin.studio"],
+  },
+  typescript: {
+    // !! WARN !! -- NOT RECOMMENDED IN PRODUCTION
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
